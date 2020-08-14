c9c8625c9c798c964bff925a0412ec8f00d7526d
JeongChanDo <djc4223@naver.com>
2020-08-15 07:25:02 +0900

    to template matching

---
+++ 11day/01_gaussian_pyramid.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("C:/Users/do/Documents/github/opencv_python/res/kimheungkook.jpg")
+lower_reso1 = cv2.pyrDown(img)
+lower_reso2 = cv2.pyrDown(lower_reso1)
+
+
+cv2.imshow("img", img);
+cv2.imshow("lower_reso1", lower_reso1);
+cv2.imshow("lower_reso2", lower_reso2);
+
+cv2.waitKey(0);
+cv2.destroyAllWindows();
+
---
+++ 12day/01_histogram.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('C:/Users/do/Documents/github/opencv_python/res/home.jpg',0)
+
+plt.subplot(121),plt.imshow(img, cmap="gray")
+plt.subplot(122),plt.hist(img.ravel(),256,[0,256]);
+
+plt.show()
